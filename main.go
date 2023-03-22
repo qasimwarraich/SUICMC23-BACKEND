e57505d9659411dbac6930efc63604ceeaabff54
qasimwarraich <qasim.warraich@gmail.com>
2023-03-22 23:55:07 +0100

    feat: Initial implementation of custom registration email

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"net/mail"
 
 	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/tools/mailer"
 )
 
 func main() {
@@ -46,6 +51,41 @@ func main() {
 
 		return nil
 	})
+
+	app.OnRecordAfterCreateRequest("participants").Add(func(e *core.RecordCreateEvent) error {
+		templateData := struct {
+			Name  string
+			URL   string
+			TWINT string
+			Logo  string
+		}{
+			Name:  e.Record.GetString("nick_name"),
+			URL:   "https://suicmc23.vercel.app/cream",
+			TWINT: "https://suicmc23.vercel.app/_app/immutable/assets/reg_twint_sm-72c3cf7b.png",
+			Logo:  "https://suicmc23.vercel.app/_app/immutable/assets/warnwest-721abf10.png",
+		}
+
+		tmpl, err := template.ParseFiles("assets/registration_email_template.html")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		buf := new(bytes.Buffer)
+		tmpl.Execute(buf, templateData)
+
+		message := &mailer.Message{
+			From: mail.Address{
+				Address: app.Settings().Meta.SenderAddress,
+				Name:    app.Settings().Meta.SenderName,
+			},
+			To:      mail.Address{Address: "example@example.com"},
+			Subject: "Thank you for registering for SUICMC23 BERN",
+			HTML:    buf.String(),
+		}
+
+		return app.NewMailClient().Send(message)
+	})
+
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
