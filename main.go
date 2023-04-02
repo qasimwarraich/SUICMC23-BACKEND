d3091508e75d116d5b58b2085d81443aafd984a8
qasimwarraich <qasim.warraich@gmail.com>
2023-04-02 19:23:16 +0200

    feat: Add automated volunteer email confirmation

--- main.go
+++ main.go
@@ -20,6 +20,12 @@ import (
 func main() {
 	app := pocketbase.New()
 
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		fmt.Println(err)
+	}
+
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		_, err := e.Router.AddRoute(echo.Route{
 			Method: http.MethodGet,
@@ -63,12 +69,6 @@ func main() {
 	})
 
 	app.OnRecordAfterCreateRequest("participants").Add(func(e *core.RecordCreateEvent) error {
-		err := godotenv.Load()
-		if err != nil {
-			fmt.Println("Error loading .env file")
-			fmt.Println(err)
-		}
-
 		nickName := e.Record.GetString("nick_name")
 		if nickName == "" {
 			nickName = e.Record.GetString("first_name")
@@ -118,6 +118,78 @@ func main() {
 			Bcc:     []mail.Address{},
 			Cc:      []mail.Address{},
 			Subject: "Thank you for registering for SUICMC23 BERN",
+			HTML:    buf.String(),
+			Text:    buf.String(),
+			Headers: map[string]string{},
+		}
+
+		return app.NewMailClient().Send(message)
+	})
+
+	app.OnRecordAfterCreateRequest("volunteers").Add(func(e *core.RecordCreateEvent) error {
+		name := e.Record.GetString("first_name")
+		emailAddress := e.Record.GetString("email")
+
+		volunteerFriday := e.Record.GetBool("volunteer_friday")
+
+		volunteerSaturdayMorning := e.Record.GetBool("volunteer_saturday_morning")
+		volunteerSaturdayAfternoon := e.Record.GetBool("volunteer_saturday_afternoon")
+		volunteerSaturdayEvening := e.Record.GetBool("volunteer_saturday_evening")
+
+		volunteerSundayMorning := e.Record.GetBool("volunteer_sunday_morning")
+		volunteerSundayAfternoon := e.Record.GetBool("volunteer_sunday_afternoon")
+		volunteerSundayEvening := e.Record.GetBool("volunteer_sunday_evening")
+
+		volunteerMondayMorning := e.Record.GetBool("volunteer_monday_morning")
+		volunteerMondayAfternoon := e.Record.GetBool("volunteer_monday_afternoon")
+
+		templateData := struct {
+			Name                       string
+			Email                      string
+			Logo                       string
+			RegEmail                   string
+			VolunteerFriday            bool
+			VolunteerSaturdayMorning   bool
+			VolunteerSaturdayAfternoon bool
+			VolunteerSaturdayEvening   bool
+			VolunteerSundayMorning     bool
+			VolunteerSundayAfternoon   bool
+			VolunteerSundayEvening     bool
+			VolunteerMondayMorning     bool
+			VolunteerMondayAfternoon   bool
+		}{
+			Name:                       name,
+			Email:                      emailAddress,
+			Logo:                       os.Getenv("VOLUNTEER_LOGO_IMAGE"),
+			RegEmail:                   os.Getenv("REG_EMAIL"),
+			VolunteerFriday:            volunteerFriday,
+			VolunteerSaturdayMorning:   volunteerSaturdayMorning,
+			VolunteerSaturdayAfternoon: volunteerSaturdayAfternoon,
+			VolunteerSaturdayEvening:   volunteerSaturdayEvening,
+			VolunteerSundayMorning:     volunteerSundayMorning,
+			VolunteerSundayAfternoon:   volunteerSundayAfternoon,
+			VolunteerSundayEvening:     volunteerSundayEvening,
+			VolunteerMondayMorning:     volunteerMondayMorning,
+			VolunteerMondayAfternoon:   volunteerMondayAfternoon,
+		}
+
+		tmpl, err := template.ParseFiles("assets/volunteer_registration_email_template.html")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		buf := new(bytes.Buffer)
+		err = tmpl.Execute(buf, templateData)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		message := &mailer.Message{
+			From:    mail.Address{Name: app.Settings().Meta.SenderName, Address: app.Settings().Meta.SenderAddress},
+			To:      []mail.Address{{Address: emailAddress}},
+			Bcc:     []mail.Address{},
+			Cc:      []mail.Address{},
+			Subject: "Thank you for registering to volunteer for SUICMC23 BERN",
 			HTML:    buf.String(),
 			Text:    buf.String(),
 			Headers: map[string]string{},
